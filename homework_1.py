34a10d0ad11b77973d5fc9d9666904744ef1d8ac
Anara <anaraberenbaeva93@gmail.com>
2025-12-05 17:50:18 +0600

    homework1

--- homework_1.py
+++ homework_1.py
@@ -4,12 +4,14 @@ class Person:
         self.birth_data = birth_date
         self.occupation = occupation
         self.higher_education = higher_education
+
+    def introduce(self):
+        education = "есть" if self.higher_education else "нет"
+        print(f" Меня зовут {self.name}, я родилась {self.birth_data}, по профессии {self.occupation}, "
+              f" высшего образования {education}")
 person1 = Person("Айдай", 29.11, "доктор", True)
 person2 = Person("Атай", 12.4, "певец", False)
 person3 = Person("Адина", 25.5, "бухгалтер", True)
-print(f" Меня зовут {person1.name}, я родилась {person1.birth_data}, по профессии {person1.occupation}, "
-      f" высшего образования есть ({person1.higher_education})")
-print(f" Меня зовут {person2.name}, я родился {person2.birth_data}, по профессии {person2.occupation}, "
-      f" высшего образования нет ({person2.higher_education})")
-print(f" Меня зовут {person3.name}, я родилась {person3.birth_data}, по профессии {person3.occupation}, "
-      f" высшего образования есть ({person3.higher_education})")
+person1.introduce()
+person2.introduce()
+person3.introduce()
